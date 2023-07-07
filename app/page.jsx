4863d8b145ee45a6df1782af66f97a62f00ce701
jatinamtewal <jatinamtewal@gmail.com>
2023-07-07 18:35:47 +0530

    added css to p tag

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <br className="max-md:hidden" />
         <span className="orange_gradient text-center">AI Powered Prompts</span>
       </h1>
-      <p>
+      <p className="desc text-center">
         Promptopia is an open-source AI Prompting tool for modern world to
         discover, create and share creative prompts.
       </p>
